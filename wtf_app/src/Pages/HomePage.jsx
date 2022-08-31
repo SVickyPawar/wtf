bff5d0657a85015d0f602c46779086ee030ba62a
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-08-31 16:52:18 +0530

    listing gym done

--- wtf_app/src/Pages/HomePage.jsx
+++ wtf_app/src/Pages/HomePage.jsx
@@ -6,16 +6,24 @@ const HomePage = () => {
 
     const [list, setList] = useState([]);
 
+    console.log(list)
+
     const url ="https://devapi.wtfup.me/gym/nearestgym?lat=30.325488815850512&long=78.0042384802231";
     useEffect(() => {
-        fetch(url).then((res) => res.json()).then(data => console.log(data.data));
+        fetch(url).then((res) => res.json()).then(data => setList(data.data))
     },[])
   return (
-      <div style={{display:"flex"}}>
-          <SideBar />
-          <GymList/>
-    </div>
-  )
+		<div style={{ display: "flex" }}>
+			<SideBar />
+			<div>
+				{list.map((el) => (
+					<div key={el.id}>
+						<GymList gym={el} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default HomePage
--- wtf_app/src/components/GymList.jsx
+++ wtf_app/src/components/GymList.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './GymList.module.css'
 
-const GymList = () => {
-  return (
-      <div className={styles.Div}>
-          <div className={styles.firstBox}>
-              <h1>Hello</h1>
-         </div>
-          <div className={styles.secondBox}>
-              <h1>Vivek</h1>
-         </div>
-    </div>
-  )
+const GymList = ({ gym }) => {
+    // const [rating, setRating] = useState(gym_rating);
+   
+    console.log(gym,"gym");
+    return (
+			<div className={styles.Div}>
+				<div className={styles.firstBox}></div>
+				<div className={styles.secondBox}>
+					<h3>{gym.gym_name}</h3>
+					<p>{gym.rating}</p>
+					<p>
+						{gym.address1} {gym.address2}
+					</p>
+					<p>{`${gym.duration_text} away | ${gym.distance} km`}</p>
+					<button className={styles.bookButton}>
+						<h3>Book Now</h3>
+					</button>
+				</div>
+			</div>
+		);
 }
 
 export default GymList
